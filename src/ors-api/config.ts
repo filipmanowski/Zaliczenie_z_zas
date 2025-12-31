c9433443107921e3b7f1168552328e498326f701
Filip Manowski <filipmanowski@gmail.com>
2025-12-31 14:12:15 +0100

    first commit

---
+++ src/ors-api/config.ts
@@ -0,0 +1,6 @@
+export default {
+    apiKey: 'eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjI4YWUxOGNlMTdiODQwYTliZGVjOGQyOTFiOGNkMmRkIiwiaCI6Im11cm11cjY0In0=',
+    reverseGeocodeUrl: 'https://api.openrouteservice.org/geocode/reverse?',
+    routeServiceUrl: 'https://api.openrouteservice.org/v2/directions/',
+    geocodeServiceUrl: 'https://api.openrouteservice.org/geocode/search?'
+}
